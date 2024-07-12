611740b570a81c74cc0cf277045a50ca3a146ec4
Knarik Nikoghosyan <92878344+KnarikNikoghosyan@users.noreply.github.com>
2024-07-12 19:48:23 +0400

    Delete Leetcode/Squares_of_a_Sorted_Array.py

--- Leetcode/Squares_of_a_Sorted_Array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        squares = [i ** 2 for i in nums]
-        squares.sort()
-        return squares
-
-res = Solution()
-nums = [-4, -1, 0, 3, 10]
-print(res.sortedSquares(nums))
