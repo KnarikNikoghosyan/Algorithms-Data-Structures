a4358474ae0e9822f3cc1be231dd44398072ac7e
Knarik Nikoghosyan <92878344+KnarikNikoghosyan@users.noreply.github.com>
2024-07-12 19:50:39 +0400

    Leetcode problems

---
+++ Leetcode/Sum_of_Two_Integers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        while b != 0:
+            sum_without_carry = a ^ b
+            carry = (a & b) << 1
+            a, b = sum_without_carry, carry
+        return a
+
+res = Solution()
+a = 1
+b = 2
+print(res.getSum(a, b))
+# time limit exceeded
